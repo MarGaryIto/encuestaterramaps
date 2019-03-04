446f7dac435c4455ca3852801ee9a860322a3646
MarGaryIto <1715110196@utec-tgo.edu.mx>
2019-03-04 12:00:31 -0600

    Update conexion.php

--- librerias/conexion.php
+++ librerias/conexion.php
@@ -1,6 +1,6 @@
 <?php
      //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
-	$mysqli = new mysqli("localhost","root","","bdencuesta"); 
+	$mysqli = new mysqli("z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","a475y0a742z9kxy5","qfq6hvbjqb8po5ki","ost4g6yz9kex2re7"); 
 	
 	if(mysqli_connect_errno()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
