709fb412103a2711f50d0e43776b985429737fee
MarGaryIto <1715110196@utec-tgo.edu.mx>
2019-03-04 12:23:38 -0600

    Update conexion.php

--- librerias/conexion.php
+++ librerias/conexion.php
@@ -1,7 +1,6 @@
 <?php
      //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
-	$mysqli = new mysqli("z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","a475y0a742z9kxy5","qfq6hvbjqb8po5ki","ost4g6yz9kex2re7"); 
-	
+	$mysqli = new mysqli("i943okdfa47xqzpy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","q923e2k5av0o6g3e","dw6rzwed0y0sbv32","nob4xoafqpyvdedq"); 
 	if(mysqli_connect_errno()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
 		exit();
