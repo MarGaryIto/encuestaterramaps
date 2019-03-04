d95564605fffbd0ef3f0f3e142a6fb24005343d9
MarGaryIto <1715110196@utec-tgo.edu.mx>
2019-03-04 12:01:50 -0600

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,9 +1,9 @@
 <?php 
 function conexion(){
-	$servidor="localhost";
-	$usuario="root";
-	$password="";
-	$bd="bdencuesta";
+	$servidor="z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+	$usuario="a475y0a742z9kxy5";
+	$password="qfq6hvbjqb8po5ki";
+	$bd="ost4g6yz9kex2re7";
 	$conexion=mysqli_connect($servidor,$usuario,$password,$bd);
 	return $conexion;
 } ?>
